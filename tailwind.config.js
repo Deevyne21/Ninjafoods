525617586093c0b13b11e514d59e24716ea66eff
Divine Johnson <johnsondivine15@gmail.com>
2025-08-26 10:55:28 -0700

    updated responsiveness

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,8 @@ export default {
     extend: {
       colors: {
         brand: "#1e40af",
+        // secondary: "#889bae",
+        secondary: "#B2BEB5",
       },
       fontFamily: {
         body: ["Nunito"],
